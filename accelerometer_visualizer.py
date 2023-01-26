b72134af56c9bac980faf64ab1ba67a8cc2ec5b2
Raleigh Littles III <raleighlittles@gmail.com>
2023-01-26 14:25:40 -0800

    Tweaking plot

--- accelerometer_visualizer.py
+++ accelerometer_visualizer.py
@@ -10,16 +10,28 @@ def create_accelerometer_plot(accel_vector : typing.List) -> str:
 
     fig = matplotlib.pyplot.figure()
 
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(projection='3d')
 
     # TODO: print `g` in LaTeX format
     ax.set_title("Acceleration (g)")
 
     ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    ax.set_xlim(-1, 1)
 
-    ax.quiver(0, 0, 0, x, y, z)
+    ax.set_ylabel("Y")
+    ax.set_ylim(-1, 1)
+
+    ax.set_zlabel("Z")
+    ax.set_zlim(-1, 1)
+
+    #ax.axis('equal')
+
+    #ax.set_aspect('equal')
+
+    ax.quiver(0, 0, 0, x, y, z, normalize=True)
+    
+    #ax.set_box_aspect([1,1,1])
+
 
     matplotlib.pyplot.savefig("figure.png")
 
